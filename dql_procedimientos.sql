dca67b8d1b80c1071f5737f8f23ca4288a1e3039
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-06-26 07:58:10 -0500

    feat: :sparkles: Registro cuota de manejo procedimiento

---
+++ dql_procedimientos.sql
@@ -0,0 +1,38 @@
+
+USE banco_cl;
+
+-- Registrar una nueva cuota de manejo calculando automáticamente el descuento.
+
+SELECT * FROM Cuotas_de_manejo;
+DELIMITER //
+DROP PROCEDURE IF EXISTS ps_registro_cuota;
+CREATE PROCEDURE ps_registro_cuota(IN p_tarjeta_id INT)
+BEGIN 
+    DECLARE _descuento_tarjeta DECIMAL(5,2);
+    DECLARE _total decimal(10,2);
+
+    SELECT tt.descuento INTO _descuento_tarjeta
+    FROM Tarjetas t
+    INNER JOIN Tipo_tarjetas tt ON tt.id = t.tipo_tarjeta_id
+    WHERE t.id = p_tarjeta_id;
+
+    SET _total = 50000.00 - (50000.00 * _descuento_tarjeta / 100);
+
+
+    INSERT INTO Cuotas_de_manejo(
+        tarjeta_id,
+        monto_base,
+        monto_total,
+        vencimiento_cuota,
+        estado
+        ) VALUES
+    (p_tarjeta_id, 50000.00, _total, DATE_ADD(CURDATE(), INTERVAL 1 MONTH), 'Pendiente');
+    
+
+END //
+DELIMITER ;
+
+CALL ps_registro_cuota(1);
+
+
+
